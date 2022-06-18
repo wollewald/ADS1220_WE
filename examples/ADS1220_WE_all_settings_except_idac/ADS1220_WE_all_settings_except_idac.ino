57cfc51545261719dbe1347b97aaf341368cea1b
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2022-06-18 21:48:24 +0200

    Add files via upload

--- examples/ADS1220_WE_all_settings_except_idac/ADS1220_WE_all_settings_except_idac.ino
+++ examples/ADS1220_WE_all_settings_except_idac/ADS1220_WE_all_settings_except_idac.ino
@@ -58,23 +58,32 @@ void setup(){
  */ 
 //  ads.setCompareChannels(ADS1220_MUX_1_3);
 
-/* You can choose a gain between 1 (default) and 128 using setGain() if PGA is enabled (default).
- * If PGA is disabled you can still choose a gain factor up to 4. If PGA is enabled, the amplified 
- * voltage shall be between AVSS + 200mV and AVDD - 200mV. Outside this range linearity drops. 
- * For details see data sheet, section 8.3.2.1.
- * The parameters you can choose are:
+/* You can choose a gain between 1 (default) and 128 using setGain() if PGA is enabled 
+ * (default). If PGA is disabled you can still choose a gain factor up to 4. If PGA is 
+ * enabled, the amplified voltage shall be between AVSS + 200mV and AVDD - 200mV. Outside 
+ * this range linearity drops. For details check the data sheet, section 8.3.2.1.
+ * 
+ * If you apply a single-ended mode (negative AINx = AVSS), PGA must be bypassed. Accordingly, 
+ * the maximum gain is 4. The library does these settings automatically.
+ * 
+ * For the measurement of reference voltages / supply voltage PGA will also be bypassed. In 
+ * this case gain is 1.
+ * 
+ * The parameters you can choose for setGain() are:
  * ADS1220_GAIN_X with X = 1,2,4,8,16,32,64 or 128
- * With getGainFactor() you can query the gain. The function returns the effective gain and not the 
- * gain set in the register. Under certain conditions these are are different. E.g. the effective gain 
- * is set to 1 when external references are measured. 
+ * 
+ * With getGainFactor() you can query the gain. The function returns the effective gain and 
+ * not the gain set in the register. Under certian conditions thes are are different. For 
+ * example, the effective gain is set to 1 when external references are measured. 
  */
 //  ads.setGain(ADS1220_GAIN_16);
 //  ads.getGainFactor(); // returns the effective gain as a byte value
 //  ads.bypassPGA(true); // true disables PGA, false enables PGA
+//  ads.isPGABypassed(); // returns true, if PGA is bypassed
 
-/* The data rate is set with setDataRate(). The data rate also depends on the operating mode and 
- * the clock. If the internal clock is used or an external clock with 4.096 MHz the data rates are
- * as follows (per second):
+/* The data rate level with setDataRate(). The data rate itself also depends on the operating 
+ * mode and the clock. If the internal clock is used or an external clock with 4.096 MHz the data 
+ * rates are as follows (per second):
  *  
  *  Level               Normal Mode      Duty-Cycle      Turbo Mode
  * ADS1220_DR_LVL_0          20               5               40         (default)
@@ -85,8 +94,8 @@ void setup(){
  * ADS1220_DR_LVL_5         600             150             1200
  * ADS1220_DR_LVL_6        1000             250             2000
  * 
- * The higher the data rate, the higher the noise (tables are provided in section 7.1 in the data sheet).
- * In single shot mode the conversion times equal the times in Normal Mode.
+ * The higher the data rate, the higher the noise (tables are provided in section 7.1 in the 
+ * data sheet). In single-shot mode the conversion times equal the times in Normal Mode.
  */
 //  ads.setDataRate(ADS1220_DR_LVL_2);  
 
@@ -97,16 +106,17 @@ void setup(){
  */
 //  ads.setOperatingMode(ADS1220_DUTY_CYCLE_MODE);
 
-/*  You can choose between a continuous and a single-shot (on demand) mode with setConversionMode();
+/*  You can choose between a continuous and a single-shot (on demand) mode with 
+ *  setConversionMode(). Parameters are:
  *  ADS1220_SINGLE_SHOT (default)
  *  ADS1220_CONTINUOUS  
  */
 // ads.setConversionMode(ADS1220_CONTINUOUS);
 
-/* In order to obtain temperature values, choose enableTemperatureSensor(true); false will disable
- * the temperature sensor. As long as the temperature sensor is enabled the ADS1220 is blocked for
- * this task. To obtain voltage values, you have to switch thesensor off. The temperature is queried 
- * with getTemperature(); 
+/* In order to obtain temperature values, choose enableTemperatureSensor(true); false will 
+ * disable the temperature sensor. As long as the temperature sensor is enabled the ADS1220 
+ * is blocked for this task. To obtain voltage values, you have to switch the sensor off. The
+ * temperature is queried with getTemperature(); 
  */
 //  ads.enableTemperatureSensor(true);
 //  ads.getTemperature(); // returns temperature as float
@@ -122,9 +132,9 @@ void setup(){
  * setVRefValue_V(vRef in volts);
  * 
  * Alternatively, you can set the reference voltage source and let the ADS1220 measure 
- * the reference. Be aware that this is not a measurement with highest precision. "Calibration" 
- * might be a bit misleading. You should take the lowest data rate (default) for most accurate 
- * results. You can use the following functions:
+ * the reference. Be aware that this is not a measurement with highest precision. 
+ * "Calibration" might be a bit misleading. You should take the lowest data rate (default) 
+ * for most accurate results. You can use the following functions:
  * setRefp0Refn0AsVefAndCalibrate();
  * setRefp1Refn1AsVefAndCalibrate();
  * setAvddAvssAsVrefAndCalibrate();
@@ -153,19 +163,20 @@ void setup(){
  * ADS1220_DOUT_DRDY   DRDY and DOUT pin indicate data readiness
  */
 //  ads.setDrdyMode(ADS1220_DOUT_DRDY);
-  
-} // end setup
+
+} // end of setup()
 
 void loop(){
 
-/* The following functions query measured values from the ADS1220. If you request values in single-shot 
- * mode, the conversion will be initiated automatically. The value will be delivered once DRDY goes LOW. 
- * This ensures that you will receive "fresh" data. This is particularly important when you change 
- * channels.  
+/* The following functions query measured values from the ADS1220. If you request values in 
+ * single-shot mode, the conversion will be initiated automatically. The value will be delivered 
+ * once DRDY goes LOW. This ensures that you will receive "fresh" data. This is particularly 
+ * important when you change channels.  
  */
 //  ads.getVoltage_mV(); // get result in millivolts
 //  ads.getVoltage_muV(); // get result in microvolts
 //  ads.getRawData();  // get raw result (signed 24 bit as long int)
 //  ads.getTemperature();  // get temperature (you need to enable the T-sensor before);
+
   delay(2000);
 }
